ce13b46b30fb787dba38d0e9d572de0a76487127
Donovan C. Young <dyoung522@gmail.com>
2014-10-23 00:32:07 -0400

    Remove title gem
    Replaced with home-grown title since I have no need for i18n in the title here.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def app_name
+    'Donovan Young'
+  end
+
+  def title
+    if controller_name == 'home'
+      app_name
+    else
+      "#{app_name} | #{content_for(:title) || controller_name.humanize}"
+    end
+  end
 end
